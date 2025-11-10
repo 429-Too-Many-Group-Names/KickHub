1cdb511b04c67b7e0fbcc7a8a0433e1f490f293c
Marcus Rodriguez <marcusrodriguez@marcus-macbook-pro.local>
2025-11-09 20:08:19 -0600

    added search bar, order summar page, price filtering, and remove item in cart functionality

--- django-app/kickhub/urls.py
+++ django-app/kickhub/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
   path("add-to-cart/", views.add_to_cart, name="add_to_cart"), 
   path("cart/", views.user_cart, name="cart"),
 
+  # ✅ ADDED
+  path("cart/update/", views.update_cart_item, name="update_cart_item"),
+  path("cart/remove/", views.remove_from_cart, name="remove_from_cart"),
+  path("checkout/summary/", views.order_summary, name="order_summary"),
 ]
